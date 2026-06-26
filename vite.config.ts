463d811c21c0eb4f683ddc9e06d6f6a4b8ae8702
Daffa <daffabdullah111@gmail.com>
2026-06-26 18:43:54 +0700

    feat: convert application to PWA and update app icons

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
 
 // https://vite.dev/config/
@@ -10,6 +11,37 @@ export default defineConfig({
     TanStackRouterVite(),
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Budgeting',
+        short_name: 'Budgeting',
+        description: 'Aplikasi Manajemen Keuangan Pribadi',
+        theme_color: '#18181b',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
@@ -26,3 +58,4 @@ export default defineConfig({
     },
   },
 })
+
